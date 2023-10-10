802b9b4df532bcf152c358a6345fc21bfaa2c49e
Pawee Tantivasdakarn <paweetantivasdakarn@Pawees-MacBook-Pro-2.local>
2023-10-10 14:17:48 +0700

    feat: only allow initilize swap from the FlashSwap contract

--- src/FlashSwap.sol
+++ src/FlashSwap.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {console2} from 'forge-std/Test.sol';
 
 interface IUniswapV2Callee {
@@ -18,16 +19,29 @@ interface IUniswapV2Pair {
 }
 
 contract FlashSwap is IUniswapV2Callee {
+  using SafeERC20 for IERC20;
   // DAI token0, WETH token1
   // uniswap use fee 0.3%
   // 30/10000 -> 0.003 in decimals which is 0.3%
+
+  error NotV2Pair();
+  error NotFromThisContract();
 
   // ================== constants ==================
   address private constant UNI_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
   address private constant UNI_V2_PAIR = 0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11;
 
   modifier onlyV2Pair() {
-    require(msg.sender == UNI_V2_PAIR, 'Not UniswapV2 Pair');
+    if (msg.sender != UNI_V2_PAIR) {
+      revert NotV2Pair();
+    }
+    _;
+  }
+
+  modifier onlyFromThisContract(address _sender) {
+    if (_sender != address(this)) {
+      revert NotFromThisContract();
+    }
     _;
   }
 
@@ -45,6 +59,7 @@ contract FlashSwap is IUniswapV2Callee {
     amounts = abi.decode(amountOutData, (uint[]));
   }
 
+  //slippage control
   function swapExactTokenIn(uint _amountIn, address _tokenIn, address _tokenOut) external {
     uint[] memory amounts = getAmountOut(_amountIn, _tokenIn, _tokenOut);
     bytes memory data = abi.encode(amounts[0], _tokenIn);
@@ -56,8 +71,8 @@ contract FlashSwap is IUniswapV2Callee {
     uint _amount0,
     uint _amount1,
     bytes calldata data
-  ) external onlyV2Pair {
+  ) external onlyV2Pair onlyFromThisContract(_sender) {
     (uint amountIn, address tokenIn) = abi.decode(data, (uint, address));
-    IERC20(tokenIn).transfer(UNI_V2_PAIR, amountIn);
+    IERC20(tokenIn).safeTransfer(UNI_V2_PAIR, amountIn);
   }
 }
